ac561ec785569daef41b4418dd652b37709441db
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2022-11-13 13:35:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,210 @@
+let arrayTables = [
+    ` <div class="d-flex flex-column align-items-center" data-aos="fade-down">
+        <h2 class="section-head">Pricing</h2>
+        <p class="section-text my-5">
+          Send Bulk Whatsapp Messages to your customers- no coding required
+        </p>
+
+        
+      </div>
+
+      <div class="row container">
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-left py-5"
+          data-aos="fade-right"
+        >
+          <div class="price-head border-bottom">
+            <p class="pkg">1500 PACKAGE</p>
+            <p class="tab-pane fade show active price-value py-4">$5.5</p>
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">1500 messages / month</p>
+            <p class="pkg-text">Up to 500 messages / day</p>
+            <p class="pkg-text">Support by Whatsapp & Email</p>
+            <p class="pkg-text text-decoration-line-through">
+              Image/Video Messages
+            </p>
+            <p class="pkg-text text-decoration-line-through">Max Media Size</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn text-decoration-none text-white"
+          >
+            Start Now</a
+          >
+        </div>
+
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-center py-5 position-relative"
+          data-aos="fade-up"
+        >
+          <div class="border-head-green position-absolute"></div>
+          <div class="price-head border-bottom">
+            <p class="pkg">3000 PACKAGE</p>
+            <p class="price-value py-4">$9.5</p>
+
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">3000 messages / month</p>
+            <p class="pkg-text">Up to 1000 messages / day</p>
+            <p class="pkg-text">Support by Whatsapp & Email</p>
+            <p class="pkg-text">Image/Video Messages</p>
+            <p class="pkg-text">Max Media Size</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn-green text-decoration-none text-dark"
+          >
+            Start Now</a
+          >
+        </div>
+
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-right py-5"
+          data-aos="fade-left"
+        >
+          <div class="price-head border-bottom">
+            <p class="pkg">1500 PACKAGE</p>
+            <p class="price-value py-4">$19</p>
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">7000 messages / month</p>
+            <p class="pkg-text">Up to 1500 messages / day</p>
+            <p class="pkg-text">Premium Support by Whatsapp & Email</p>
+            <p class="pkg-text">Image/Video Messages</p>
+            <p class="pkg-text">Max Media Size 30MB</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn text-decoration-none text-white"
+          >
+            Start Now</a
+          >
+        </div>
+      </div>
+      <p class="text-md-start pt-3">
+        For more info on how it works,
+        <a href="https://elwhats.com/howitworks" class="text-decoration-none"
+          >click here</a
+        >
+      </p>`,
+    
+    `  <div class="d-flex flex-column align-items-center" data-aos="fade-down">
+        <h2 class="section-head">Pricing</h2>
+        <p class="section-text my-5">
+          Send Bulk Whatsapp Messages to your customers- no coding required
+        </p>
+
+       
+      </div>
+
+      <div class="row container">
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-left py-5"
+          data-aos="fade-right"
+        >
+          <div class="price-head border-bottom">
+            <p class="pkg">1500 PACKAGE</p>
+            <p class="tab-pane fade show active price-value py-4">$60</p>
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">1500 messages / month</p>
+            <p class="pkg-text">Up to 500 messages / day</p>
+            <p class="pkg-text">Support by Whatsapp & Email</p>
+            <p class="pkg-text text-decoration-line-through">
+              Image/Video Messages
+            </p>
+            <p class="pkg-text text-decoration-line-through">Max Media Size</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn text-decoration-none text-white"
+          >
+            Start Now</a
+          >
+        </div>
+
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-center py-5 position-relative"
+          data-aos="fade-up"
+        >
+          <div class="border-head-green position-absolute"></div>
+          <div class="price-head border-bottom">
+            <p class="pkg">3000 PACKAGE</p>
+            <p class="price-value py-4">$104</p>
+
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">3000 messages / month</p>
+            <p class="pkg-text">Up to 1000 messages / day</p>
+            <p class="pkg-text">Support by Whatsapp & Email</p>
+            <p class="pkg-text">Image/Video Messages</p>
+            <p class="pkg-text">Max Media Size</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn-green text-decoration-none text-dark"
+          >
+            Start Now</a
+          >
+        </div>
+
+        <div
+          class="col-12 col-lg-4 col-md-6 price-box text-center border pkg-right py-5"
+          data-aos="fade-left"
+        >
+          <div class="price-head border-bottom">
+            <p class="pkg">1500 PACKAGE</p>
+            <p class="price-value py-4">$209</p>
+            <p class="period pb-4">PER MONTH</p>
+          </div>
+          <div class="pkg-content py-4">
+            <p class="pkg-text">7000 messages / month</p>
+            <p class="pkg-text">Up to 1500 messages / day</p>
+            <p class="pkg-text">Premium Support by Whatsapp & Email</p>
+            <p class="pkg-text">Image/Video Messages</p>
+            <p class="pkg-text">Max Media Size 30MB</p>
+          </div>
+          <a
+            href="https://elwhats.com/register"
+            class="price-btn text-decoration-none text-white"
+          >
+            Start Now</a
+          >
+        </div>
+      </div>
+      <p class="text-md-start pt-3">
+        For more info on how it works,
+        <a href="https://elwhats.com/howitworks" class="text-decoration-none"
+          >click here</a
+        >
+      </p>`];
+
+
+                let month = document.getElementById("month");
+                let year  = document.getElementById("year");
+                let tables = document.getElementById("tables");
+                month.addEventListener("click",() => {
+
+                  tables.innerHTML=arrayTables[0];
+                  month.style.backgroundColor="blue";
+                year.style.backgroundColor="white";
+
+                }
+                );
+
+
+
+                 year.addEventListener("click",() => {
+
+                  tables.innerHTML=arrayTables[1];
+                  year.style.backgroundColor="blue";
+                month.style.backgroundColor="white";
+
+                }
+                )
